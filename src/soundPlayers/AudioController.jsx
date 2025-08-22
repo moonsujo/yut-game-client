3d2ce1bb12f668550b84c58fd75975e3b849cd72
moon-rhino <jojuno@ucla.edu>
2025-08-21 19:16:01 -0700

    set resuming audio on context switch for iphone-chrome

--- src/soundPlayers/AudioController.jsx
+++ src/soundPlayers/AudioController.jsx
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import { AudioListener, AudioLoader } from "three";
-import { audioLoaderAtom, listenerAtom } from "../GlobalState";
+import { audioContextAtom, audioLoaderAtom, listenerAtom } from "../GlobalState";
 import { useAtom, useSetAtom } from "jotai";
 
 export default function AudioController() {
@@ -11,12 +11,35 @@ export default function AudioController() {
   // keep these across renders
   const [listener, setListener] = useAtom(listenerAtom);
   const setAudioLoader = useSetAtom(audioLoaderAtom);
+    const [audioContext, setAudioContext] = useAtom(audioContextAtom)
 
   useEffect(() => {
-    setListener(new AudioListener())
+    const newListener = new AudioListener()
+    setListener(newListener)
     setAudioLoader(new AudioLoader())
+        
+    const audioContext = newListener.context;
+    setAudioContext(audioContext)
+
     return () => {
       camera.remove(listener)
     };
   }, [camera]);
+
+  
+  useEffect(() => {
+    const resume = () => {
+      if (audioContext && audioContext.state === "suspended") {
+        audioContext.resume();
+      }
+    };
+
+    document.addEventListener("visibilitychange", resume);
+    window.addEventListener("focus", resume);
+
+    return () => {
+      document.removeEventListener("visibilitychange", resume);
+      window.removeEventListener("focus", resume);
+    }
+  }, [audioContext])
 }
