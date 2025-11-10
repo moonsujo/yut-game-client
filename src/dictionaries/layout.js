e73f7a82d01a03ae4e8fcfd4ac05ba397749ee9a
moon-rhino <beatrhino@gmail.com>
2025-11-09 20:40:18 -0800

    move showroom in landscape to make it fit in the window

--- src/dictionaries/layout.js
+++ src/dictionaries/layout.js
@@ -3093,7 +3093,7 @@ export default {
       },
     },
     showroom: {
-      position: [0,0,0],
+      position: [-1,0,0],
       rotation: [0,0,0],
       scale: 1,
       yutAlert: {
@@ -3181,7 +3181,7 @@ export default {
             position: [0, 0, -3],
             scale: 0.6
           },
-          beamDustPosition: [-7, 0, 2.2]
+          beamDustPosition: [-7.55, 0, 2.6]
         },
         rocketsLose: {
           position: [-1.8, 2, 1.5],
@@ -3204,45 +3204,45 @@ export default {
         position: [-4,-1,0]
       },
       rocketsWinPreview: {
-        position: [-4, 10, 4],
+        position: [-3, 10, 4],
         scale: 1,
         backButton: {
-          position: [10.8, 0, 1.1],
+          position: [11, 0, 1.5],
           rotation: [0, Math.PI, 0],
-          scale: 1.3
+          scale: 1.1
         },
         fireworks: {
           position: [-4, 8, 2]
         }
       },
       ufosWinPreview: {
-        position: [-4, 10, 4],
+        position: [-3, 10, 4],
         scale: 1,
         backButton: {
-          position: [10.8, 0, 1.1],
+          position: [11, 0, 1.5],
           rotation: [0, Math.PI, 0],
-          scale: 1.3
+          scale: 1.1
         },
         fireworks: {
           position: [-4, 8, 2]
         }
       },
       rocketsLosePreview: {
-        position: [-4, 10, 4],
+        position: [-3, 10, 4],
         scale: 1,
         backButton: {
-          position: [10.8, 0, 1.1],
+          position: [11, 0, 1.5],
           rotation: [0, Math.PI, 0],
-          scale: 1.3
+          scale: 1.1
         }
       },
       ufosLosePreview: {
-        position: [-4, 10, 4],
+        position: [-3, 10, 4],
         scale: 1,
         backButton: {
-          position: [10.8, 0, 1.1],
+          position: [11, 0, 1.5],
           rotation: [0, Math.PI, 0],
-          scale: 1.3
+          scale: 1.1
         }
       }
     },
